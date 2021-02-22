ca24408e48347f1dfb6e936e5b3ad728acea9ba3
Jackson Graff <jacksongraff@hotmail.com>
2021-02-22 16:08:25 -0300

    Aula 1
    Código desenvolvido durante a primeria aula do evento #NLW4

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+
+const app = express();
+
+/**
+ * GET => Busca
+ * POST => Salvar
+ * PUT => Alterar
+ * DELETE => Deletar
+ * PATCH => Alteração Específica
+ */
+
+// http://localhost:3333/users
+app.get('/', (request, response) => {
+    return response.json({ message: "Hello World - NLW04" })
+})
+
+// 1 param => Rota(Recusro API)
+// 2 param => request, response
+app.post('/', (request, response) => {
+    return response.json({ message: 'Os dados foram salvos com sucesso!' })
+})
+
+app.listen(3333, () => console.log('Server is running!'));
