a2c8c2731a08a32a054391763c32f4cb5a84ca13
Jackson Graff <jacksongraff@hotmail.com>
2021-02-26 16:53:12 -0300

    Aula 5
    Código desenvolvido durante a ultima aula do evento #NLW4

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -6,7 +6,7 @@ import { UsersRepository } from '../repositories/UsersRepository';
 import { SurveysRepository } from '../repositories/SurveysRepository';
 import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
 import SendMailService from '../services/SendMailService';
-import { Survey } from '../models/Survey';
+import { AppError } from '../../errors/AppError';
 
 class SendMailController {
 
@@ -26,14 +26,14 @@ class SendMailController {
 
         const survey = await surveysRepository.findOne({ id: survey_id })
 
+
         if (!survey) {
-            return response.status(400).json({
-                error: "Survey does not exists"
-            })
+            throw new AppError('Survey does not exists!')
         }
 
+
         const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
-            where: [{ user_id: user.id }, { value: null }],
+            where: { user_id: user.id, value: null },
             relations: ['user', 'survey']
         })
 
@@ -43,11 +43,12 @@ class SendMailController {
             name: user.name,
             title: survey.title,
             description: survey.description,
-            user_id: user.id,
+            id: '',
             link: process.env.URL_MAIL
         }
 
         if (surveyUserAlreadyExists) {
+            variables.id = surveyUserAlreadyExists.id
             await SendMailService.execute(email, survey.title, variables, npsPath);
             return response.json(surveyUserAlreadyExists);
         }
@@ -58,6 +59,7 @@ class SendMailController {
         })
 
         await surveysUsersRepository.save(surveyUser)
+        variables.id = surveyUser.id
 
         await SendMailService.execute(email, survey.title, variables, npsPath)
 
